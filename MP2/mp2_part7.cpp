ae12230bcba3363b9324fe4015630f6f1fce378a
ChaseElander <chaseelander@gmail.com>
2017-02-17 23:23:47 -0600

    working ls, need to continue through 2nd crap

--- MP2/mp2_part7.cpp
+++ MP2/mp2_part7.cpp
@@ -4,55 +4,83 @@
 /*                                                                            */
 /* File: ./mp3_part7.cpp                                                      */
 /* -------------------------------------------------------------------------- */
-/*Programmer:Chase Elander                                                    */
+
 /* -------------------------------------------------------------------------- */
 /* Standard Library Includes                                                  */
 /* -------------------------------------------------------------------------- */
-
 #include <string.h>
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <iostream>
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <errno.h>
+#include<sys/types.h>
+
+using namespace std;
 
 int main()
 {
-
-
-  //TODO Fix basiclaly everything.
-	int filedes[2];
+   
+    int filedes[2];
     //char buf[PIPE_BUF];
     //char buf = [PIPE_BUF];
     //ssize_t numBytes;
     
     pipe(filedes); // filedes[0] is for reading, filedes[1] is for writing.
-    pid_t pid = fork();
-    if (pid < 0) {
-        std::cout << "ERROR" << strerror(errno) <<std::endl; 
-    }else if(pid == 0){//allows only child into IF
-        close(filedes[0]); //close unused read
-        dup2(filedes[1], 1);
-        close(filedes[1]);
-        char* arg[] = {"ls", "-la", NULL};
-        execvp("ls", arg); //only child runs -la -ls 
-        if (fork() == 0){
-        	
-        }        
-    }else{
-        //waitpid(pid, NULL, 0); //Parent waits for child to complete and send temp
+    
+    if(fork() == 0){//allows only child into IF
+        
+        int filedes2[2];
+        pipe(filedes2);
+        
+        if(fork() == 0){ //grandchild only
+            //close(filedes2[0]); //close unused read
+            dup2(filedes2[1], 1);// go into filedes2
+            //dup2(filedes2[0], 1);   //This?  0 is for reading 1 is for writing
+            /*close(filedes2[0]);
+            dup2(filedes2[1], 1); //Something like this
+            close(filedes2); */
+            close(filedes2[1]); //close pipe[1]
+            char* arg[] = {"tr", "[a-zA-Z0-9]", "a", NULL};
+            execvp("tr", arg);
+            
+        }
+        else{  // Child only, not grandchild
+            execl("/bin/ls", "/bin/ls", "-la", NULL); //only grandchild runs -la -ls
+            //Runs the 'ls -la', now must send the results to the child to run next command
+
+            //close(filedes2[1]);
+            //dup2 here
+            //char c;
+           
+           
+           
+            wait(NULL); //Parent waits for child to complete and send temp
+            close(filedes2[1]);
+            char c;
+            while((read(filedes2[0], &c, 1))!=0 ){   //while read from filedes2 hasn't finished
+                write(filedes[0], &c, 1);//write to filedes
+            }
+           
+           
+            /* After (tr [a-zA-Z0-9] a) has begun running, send the results of the above
+                write to that process. Send output to parent for STDOUT output */
+            
+        }
+        
+        close(filedes2[0]);
+    }else{  //Final parent ONLY
+        wait(NULL); //Parent waits for child to complete and send temp
         close(filedes[1]);
         char c;
         while((read(filedes[0], &c, 1))!=0 ){   //not exactly sure what the 1 means
+           //numBytes = read(filedes[0], buf, sizeof(buf));
            write(STDOUT_FILENO, &c, 1);
+          // break; //horrible practice but it works dammit 
         }
     }
 
-
-
-
+    close(filedes[0]);
     exit(0);
 }
 
